1452761287d35cb707dd1943daabe0f7e5948cc1
VictorASF <vasf111@hotmail.com>
2021-09-23 01:17:20 -0300

    Change data to dia Novo arquivo Execute

--- createTable.py
+++ createTable.py
@@ -1,12 +1,12 @@
 import connectionDatabase as Conn
 
+
 # docker run --name=Mysql56 -e MYSQL_ROOT_PASSWORD=root -p 3308:3306 -d mysql:5.6
 
 # Metodo que cria uma tabela caso não exista
 
 
 def criarTabela(nome):
-
     # Metodo do arquivo connectionDatabase.py que serve para se conectar ao banco de dados MySql
     database = Conn.connectionDB()
 
@@ -20,3 +20,15 @@ def criarTabela(nome):
     fechamento DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (id));""")
     database.commit()
+
+
+def criarTabelaDividendo(nome):
+    database = Conn.connectionDB()
+
+    cursor = database.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS dividend." + nome + """(
+    id INT NOT NULL AUTO_INCREMENT,
+    mes DATE NOT NULL,
+    yield FLOAT NOT NULL,
+    PRIMARY KEY (id));""")
--- execute.py
+++ execute.py
@@ -4,7 +4,7 @@ database = Conn.connectionDB()
 
 cursor = database.cursor()
 
-cod_fii = 'HGLG11'
+cod_fii = 'BTLG11'
 
 # Opções de FII
 # BTLG11 Imovel Logistico
@@ -19,33 +19,41 @@ cod_fii = 'HGLG11'
 # MFII11 Fundo de Desenvolvimento
 # BPFF11 Fundo de Fundos
 
+valor_investido = 10000
 
 dia_inicio = '2017-01-01'
 
 dia_fim = '2021-09-06'
 
-# cursor.execute('SELECT count(fechamento) from '+cod_fii+' WHERE dia >="'+dia_inicio+'" and dia <="'+dia_fim+'";')
-# N = cursor.fetchone()
-# N = (N[0])
-
 # Select no banco para pegar os valores de abertura do fundo nos dias indicados
 cursor.execute(
-    'SELECT dia, fechamento from ' + cod_fii + ' WHERE dia >="' + dia_inicio + '" and dia <="' + dia_fim + '";')
+    'SELECT dia, fechamento from readCSV.' + cod_fii + ' WHERE dia >="' + dia_inicio + '" and dia <="' + dia_fim + '";')
 fii = cursor.fetchall()
 
 # Select no banco para pegar o valor de abertura do IFIX(benchmark) nos dias indicados
-cursor.execute('SELECT dia, fechamento from IFIX WHERE dia >="' + dia_inicio + '" and dia <="' + dia_fim + '";')
+cursor.execute('SELECT dia, fechamento from readCSV.IFIX WHERE dia >="' + dia_inicio + '" and dia <="' + dia_fim + '";')
 ifix = cursor.fetchall()
 
-cursor.execute('SELECT dia from IFIX WHERE dia >="' + dia_inicio + '" and dia <="' + dia_fim + '";')
+cursor.execute('SELECT dia from readCSV.IFIX WHERE dia >="' + dia_inicio + '" and dia <="' + dia_fim + '";')
 dias = cursor.fetchall()
 
-cursor.execute('SELECT count(dia) from IFIX WHERE dia >="' + dia_inicio + '" and dia <="' + dia_fim + '";')
+cursor.execute('SELECT count(dia) from readCSV.IFIX WHERE dia >="' + dia_inicio + '" and dia <="' + dia_fim + '";')
 contador = cursor.fetchall()
-print(contador)
 contador = (contador[0][0])
 
-print(contador)
+cursor.execute('SELECT timestampdiff(MONTH,"' + dia_inicio + '", "' + dia_fim + '") from readCSV.IFIX;')
+meses = cursor.fetchall()
+meses = (meses[0][0])
+
+cursor.execute(
+    'SELECT yield from dividend.' + cod_fii + ' WHERE mes >="' + dia_inicio + '" and mes <="' + dia_fim + '";')
+
+yieldList = cursor.fetchall()
+
+sumYield = 0
+
+for i in yieldList:
+    sumYield += i[0]
 
 # Variavel auxiliar para limpar qualquer dia incluso onde a bolsa não funcionou
 dia_auxiliar = []
@@ -75,19 +83,7 @@ vet_fii.reverse()
 
 vet_return_ifix = []
 vet_return_fii = []
-
-# i = 0
-
-# for i in range(contador-1):
-#    vet_return_ifix.append(1 - (vet_ifix[i] / vet_ifix[i + 1]))
-#    i += 1
-
-# i = 0
-
-# for i in range(contador-1):
-#    vet_return_fii.append(1 - (vet_fii[i] / vet_fii[i + 1]))
-#    i += 1
-
+vet_preco_medio = 0
 
 for i in range(len(dias) - 1):
     if i + 1 < len(dias):
@@ -96,6 +92,11 @@ for i in range(len(dias) - 1):
 for i in range(len(dias) - 1):
     if i + 1 < len(dias):
         vet_return_fii.append(1 - (vet_fii[i] / vet_fii[i + 1]))
+
+for i in range(len(dias) - 1):
+        vet_preco_medio += vet_fii[i]
+
+vet_preco_medio = float(vet_preco_medio/len(dias))
 
 media_return_fii = 0
 media_return_ifix = 0
@@ -108,8 +109,8 @@ for x in vet_return_fii:
     media_return_fii += x
 media_return_fii = media_return_fii / (len(vet_return_fii))
 
-print(f"Retorno media FII: {media_return_fii}")
-print(f"Retorno media IFIX: {media_return_ifix}")
+# print(f"Retorno media FII: {media_return_fii}")
+# print(f"Retorno media IFIX: {media_return_ifix}")
 
 COV = 0
 VAR = 0
@@ -118,18 +119,18 @@ for x in range(len(vet_return_fii)):
     COV += ((vet_return_fii[x] - media_return_fii) * (vet_return_ifix[x] - media_return_ifix))
 COV = COV / (len(vet_return_fii))
 
-print(f"Covariancia: {COV}")
+# print(f"Covariancia: {COV}")
 
 for x in range(len(vet_return_ifix)):
     VAR += (vet_return_ifix[x] - media_return_ifix) ** 2
 VAR = VAR / (len(vet_return_ifix) - 1)
 
-print(f"Variancia: {VAR}")
+# print(f"Variancia: {VAR}")
 
 Beta = COV / VAR
 Beta = float(Beta)
 
-print(f"Indice Beta: {Beta}")
+print(f"Indice Beta {cod_fii}: {Beta:0.4f}")
 
 # Retorno calculo do retorno IFIX
 retorno_ifix = ((vet_ifix[0] / vet_ifix[len(vet_ifix) - 1]) - 1)
@@ -139,5 +140,25 @@ retorno_ifix = ((vet_ifix[0] / vet_ifix[len(vet_ifix) - 1]) - 1)
 
 retorno_fii = ((vet_fii[0] / vet_fii[len(vet_fii) - 1]) - 1)
 
-print(f'{retorno_ifix * 100}')
-print(f'{retorno_fii * 100}')
+# CAPM
+
+returnFreeOfRisk = (5.25 / 100)
+
+returnBenchmark = float(retorno_ifix)
+
+returnExpected = returnFreeOfRisk + (Beta * (returnBenchmark - returnFreeOfRisk))
+print(f'Retorno Esperado (CAPM) do FII {cod_fii}: {returnExpected * 100:05.2f}%')
+print(f'Retorno IFIX: {retorno_ifix * 100:05.2f}%')
+print(f'Retorno real do FII {cod_fii}: {retorno_fii * 100:05.2f}%')
+print(
+    f'Retorno sobre um investimento de R${valor_investido}: R${valor_investido + (retorno_fii * valor_investido):0.2f}')
+
+print(f'Retorno em redimentos {cod_fii} com o valor de R${valor_investido} investido:')
+
+# Dividendos
+cotas = float(valor_investido//vet_fii[-1])
+rendimentos = float((sumYield/100)*vet_preco_medio)
+print(f'No ato da compra geraria {cotas} cotas que renderam cada cota {rendimentos:0.2f}')
+print(f'Ou R${cotas*rendimentos:0.2f} no total')
+
+
